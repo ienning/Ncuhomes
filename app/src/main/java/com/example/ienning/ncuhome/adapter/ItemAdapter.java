dca14c4d220194765bcbb29f2a7f04fb3fe744c4
ienning <931208707@qq,com>
2016-07-16 12:00:09 +0800

    try

---
+++ app/src/main/java/com/example/ienning/ncuhome/adapter/ItemAdapter.java
@@ -0,0 +1,23 @@
+package com.example.ienning.ncuhome.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by ienning on 16-7-16.
+ */
+public class ItemAdapter extends RecyclerView.Adapter {
+    private static final int ITEM_VIEW_TYPE_ITEM = 0;
+    private List<ItemArticle> dataList;
+    public ItemAdapter<List<ItemArticle> dataList> {
+        this.dataList = dataList;
+    }
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        try {
+            return
+        }
+    }
+}
