a9d269046e7553e7b997fab90e15fa44fd28e32d
ienning <931208707@qq,com>
2016-07-17 00:42:42 +0800

    实现大概布局

---
+++ app/src/main/java/com/example/ienning/ncuhome/personcenter/PersonCenter.java
@@ -0,0 +1,17 @@
+package com.example.ienning.ncuhome.personcenter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.ienning.ncuhome.R;
+
+/**
+ * Created by ienning on 16-7-17.
+ */
+public class PersonCenter extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_center);
+    }
+}
