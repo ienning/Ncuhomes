da65dc29418da8626834192a72117d8be7ae6c7f
ienning <931208707@qq,com>
2016-10-29 20:55:27 +0800

    finish change picture

--- app/src/main/java/com/example/ienning/ncuhome/personcenter/Phonenumber.java
+++ app/src/main/java/com/example/ienning/ncuhome/personcenter/Phonenumber.java
@@ -2,13 +2,24 @@ package com.example.ienning.ncuhome.personcenter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ienning.ncuhome.R;
+import com.example.ienning.ncuhome.net.HttpUtil;
+import com.example.ienning.ncuhome.net.JsonAnalysis;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * Created by ienning on 16-8-1.
@@ -18,6 +29,9 @@ public class Phonenumber extends Activity{
     private LinearLayout back;
     private TextView centerTitle;
     private Button changePhonenumber;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    private String responseStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,7 @@ public class Phonenumber extends Activity{
         centerTitle = (TextView) findViewById(R.id.center_title);
         changePhonenumber = (Button) findViewById(R.id.change_phonenumber);
         centerTitle.setText("手机号码");
+        getPhoneNumber();
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,7 +53,30 @@ public class Phonenumber extends Activity{
         changePhonenumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(Phonenumber.this, "请到云家园网页版上更换绑定的手机号码", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void getPhoneNumber() {
+        HttpUtil httpUtil = new HttpUtil();
+        sharedPreferences = getSharedPreferences("ncuhome", MODE_PRIVATE);
+        JsonAnalysis jsonAnalysis = new JsonAnalysis();
+        httpUtil.get("http://www.ncuos.com/api/user/phone_num", sharedPreferences.getString("token", null), new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
 
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                responseStr = response.body().string();
+                Log.i("Ienning", "onResponse: phonenum is " + responseStr);
+                Phonenumber.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showPhonenumber.setText(responseStr.substring(1, responseStr.length() - 2));
+                    }
+                });
             }
         });
     }
